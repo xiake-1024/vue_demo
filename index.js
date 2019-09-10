2358eae415af51a5dc4011a7a39e43aaafc15cb2
xiake-1024 <1306027770@qq.com>
2019-09-10 14:26:02 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function Vue(options) {
+  console.log(options)
+  if (!(this instanceof Vue)
+  ) {
+    warn('Vue is a constructor and should be called with the `new` keyword');
+  }
+  this._init(options);
+}
+
+initMixin(Vue)
+
+export default Vue
+
